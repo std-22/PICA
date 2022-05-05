960c5cda0a1039e1ecf204a6ba2ace3bb048b93a
MelikbekyanAshot <melikbekyan.ashot@yandex.ru>
2022-05-05 22:48:12 +0300

    Grid implementation

--- main_app.py
+++ main_app.py
@@ -1,60 +1,61 @@
 import random
 from style_transfer import StyleTransfer
-import streamlit as sl
+import streamlit as st
 from PIL import Image
 import os
-import shutil
 
 
 def set_appearance():
     """Configurate web-site settings."""
-    sl.set_page_config(page_title='PICA',
+    st.set_page_config(page_title='PICA',
                        page_icon=Image.open('assets/Pica_logo_plus.jpg'),
                        layout="wide")
-    sl.title('PICA')
-    sl.markdown(""" <style>
+    st.title('PICA')
+    st.markdown(""" <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     </style> """, unsafe_allow_html=True)
 
 
 set_appearance()
-scaler = sl.slider('Intensity', 0, 100)
+scaler = st.slider('Intensity', 0, 100)
 style_transfer = StyleTransfer()
 
 if not os.path.isdir('generated_images'):
     os.mkdir('generated_images')
 
-col1, col2, col3 = sl.columns(3)
+col1, col2, col3 = st.columns(3)
 with col1:
-    src_image = sl.file_uploader(label='Source image', type=['png', 'jpg', 'webp'])
+    src_image = st.file_uploader(label='Source image', type=['png', 'jpg', 'webp'])
     if src_image:
         src_image = Image.open(src_image)
-        sl.image(src_image, caption='Source image')
+        st.image(src_image, caption='Source image')
 
 with col2:
-    style_image = sl.file_uploader(label='Style image', type=['png', 'jpg', 'webp'])
+    style_image = st.file_uploader(label='Style image', type=['png', 'jpg', 'webp'])
     if style_image:
         style_image = Image.open(style_image)
-        sl.image(style_image, caption='Style image')
+        st.image(style_image, caption='Style image')
 
 with col3:
-    if sl.button('Generate') and style_image and src_image:
+    if st.button('Generate') and style_image and src_image:
         stylized_image = style_transfer.transfer_style(src_image, style_image, scaler / 100 * (1080 - 360) + 360)
-        sl.image(stylized_image)
+        st.image(stylized_image)
         stylized_image.save(f'generated_images/{str(random.randint(0, 10000))}.png')
 
-if sl.button('Clean history'):
+if st.button('Clean history'):
     for image in os.listdir('generated_images'):
         os.remove(f'generated_images/{image}')
 
 if len(os.listdir('generated_images')) > 0:
-    for col, image in zip(sl.columns(len(os.listdir('generated_images'))), os.listdir('generated_images')):
-        with col:
-            style_image = Image.open('generated_images/' + image)
-            sl.image(style_image, caption='Stylized image', use_column_width='always')
-            with open('generated_images/' + image, 'rb') as file:
-                btn = sl.download_button(label='Download',
+    stylized_images = [Image.open('generated_images/' + image) for image in os.listdir('generated_images')]
+    COLS_IN_GRID = 5
+    cols = st.columns(COLS_IN_GRID)
+    for index, image in enumerate(stylized_images):
+        with cols[index % COLS_IN_GRID]:
+            st.image(stylized_images[index], caption='Stylized image', use_column_width='always')
+            with open(image.filename, 'rb') as file:
+                btn = st.download_button(label='Download',
                                          data=file,
                                          file_name=f'stylized_{image}',
                                          mime='image/png',
